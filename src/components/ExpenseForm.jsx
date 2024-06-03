626269652099002f45ada0df74939a83d5b952b1
kazi Arif <coderkaziarif@gmail.com>
2024-06-04 01:24:29 +0600

    advance formula added to get input form data

--- src/components/ExpenseForm.jsx
+++ src/components/ExpenseForm.jsx
@@ -1,30 +1,63 @@
+import { useState } from "react";
+
 export default function ExpenseForm({ setExpenses }) {
+  const [expense, setExpense] = useState({
+    title: "",
+    category: "",
+    amount: "",
+  });
+
   const handleSubmit = (e) => {
     e.preventDefault();
-    const expense = { ...getFormData(e.target), id: crypto.randomUUID() };
-    setExpenses((prevState) => [...prevState, expense]);
-    e.target.reset();
+    setExpenses((prevState) => [
+      ...prevState,
+      { id: crypto.randomUUID(), ...expense },
+    ]);
+
+    setExpense({
+      title: "",
+      category: "",
+      amount: "",
+    });
   };
 
-  const getFormData = (form) => {
-    const formData = new FormData(form);
-    const data = {};
-    for (const [key, value] of formData.entries()) {
-      data[key] = value;
-    }
-    return data;
-  };
+  //^ Old version of code========>
+  //   const getFormData = (form) => {
+  //     const formData = new FormData(form);
+  //     const data = {};
+  //     for (const [key, value] of formData.entries()) {
+  //       data[key] = value;
+  //     }
+  //     return data;
+  //   };
 
   return (
     <form className="expense-form" onSubmit={handleSubmit}>
       <div className="input-container">
         <label htmlFor="title">Title</label>
-        <input id="title" name="title" />
+        <input
+          id="title"
+          name="title"
+          value={expense.title}
+          onChange={(e) =>
+            setExpense((prevState) => ({ ...prevState, title: e.target.value }))
+          }
+        />
       </div>
       <div className="input-container">
         <label htmlFor="category">Category</label>
 
-        <select id="category" name="category">
+        <select
+          id="category"
+          name="category"
+          value={expense.category}
+          onChange={(e) =>
+            setExpense((prevState) => ({
+              ...prevState,
+              category: e.target.value,
+            }))
+          }
+        >
           <option value="" hidden>
             Select Category
           </option>
@@ -37,7 +70,17 @@ export default function ExpenseForm({ setExpenses }) {
       </div>
       <div className="input-container">
         <label htmlFor="amount">Amount</label>
-        <input id="amount" name="amount" />
+        <input
+          id="amount"
+          name="amount"
+          value={expense.amount}
+          onChange={(e) =>
+            setExpense((prevState) => ({
+              ...prevState,
+              amount: e.target.value,
+            }))
+          }
+        />
       </div>
       <button className="add-btn">Add</button>
     </form>
