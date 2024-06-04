928e0f883dc3eab57ef4a45203961f36e8105880
kazi Arif <coderkaziarif@gmail.com>
2024-06-05 03:49:00 +0600

    contextMenu adding and item deleting functionality work done

--- src/components/ContextMenu.jsx
+++ src/components/ContextMenu.jsx
@@ -1,8 +1,39 @@
-export default function ContextMenu() {
+import { toast } from "react-toastify";
+export default function ContextMenu({
+  menuPosition,
+  setMenuPosition,
+  setExpenses,
+  rowId,
+}) {
+  if (!menuPosition?.left) return;
+
+  const handleDelete = () => {
+    setExpenses((prevState) => {
+      const itemToDelete = prevState.find((expense) => expense.id == rowId);
+
+      if (itemToDelete) {
+        console.log(
+          `Deleting item with id: ${rowId}, title: ${itemToDelete.title}`
+        );
+      }
+      toast.error(`Deleting item title: ${itemToDelete.title}`);
+      return prevState.filter((expense) => expense.id !== rowId);
+    });
+
+    setMenuPosition({});
+  };
+
   return (
-    <div className="context-menu">
-      <div>Edit</div>
-      <div>Delete</div>
+    <div className="context-menu" style={{ ...menuPosition }}>
+      <div
+        onClick={() => {
+          console.log("Editing");
+          setMenuPosition({});
+        }}
+      >
+        Edit
+      </div>
+      <div onClick={handleDelete}>Delete</div>
     </div>
   );
 }
