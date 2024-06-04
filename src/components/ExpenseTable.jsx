b4a713c4fed8d8d0926b3129ea119a8b383a6c4e
kazi Arif <coderkaziarif@gmail.com>
2024-06-05 00:26:21 +0600

    normal filter functin added on expense table.

--- src/components/ExpenseTable.jsx
+++ src/components/ExpenseTable.jsx
@@ -1,11 +1,18 @@
+import { useState } from "react";
+
 export default function ExpenseTable({ expenses }) {
+  const [category, setCategory] = useState("");
+  const filteredData = expenses.filter((expense) => {
+    return expense.category.toLowerCase().includes(category);
+  });
+
   return (
     <table className="expense-table">
       <thead>
         <tr>
           <th>Title</th>
           <th>
-            <select>
+            <select onChange={(e) => setCategory(e.target.value.toLowerCase())}>
               <option value="">All</option>
               <option value="grocery">Grocery</option>
               <option value="clothes">Clothes</option>
@@ -40,7 +47,7 @@ export default function ExpenseTable({ expenses }) {
         </tr>
       </thead>
       <tbody>
-        {expenses.map(({ id, title, category, amount }) => (
+        {filteredData.map(({ id, title, category, amount }) => (
           <tr key={id}>
             <td>{title}</td>
             <td>{category}</td>
